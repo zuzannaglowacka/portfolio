6b159bac31d1f0db81ca9c70fdda31b680f8a1e8
Zuzanna Glowacka <zuzglowacka@gmail.com>
2018-03-18 23:40:02 +0000

    Force SSL traffic

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php include_once("index.html"); ?>
+
+
+<?php
+
+//If the HTTPS is not found to be "on"
+if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
+{
+    //Tell the browser to redirect to the HTTPS URL.
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    //Prevent the rest of the script from executing.
+    exit;
+}
